196f149a45529bd9446d822f5ff52339c261e671
willGyu <jgt8731@itwillbs.co.kr>
2025-12-31 14:07:04 +0900

    mallapi 프로젝트 생성

---
+++ mallapi/src/test/java/com/itwillbs/mallapi/MallapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwillbs.mallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
